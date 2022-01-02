86c9a72cdc8950e51eeb5003666dd729addd9e90
Mustafa M. A. U. AbuGhazy <moustafa.abu.ghazy@gmail.com>
2022-01-02 08:58:13 +0200

    Adding Mapping for the Xtender serial protocol appendix document
    4O3C\\Technical specification - Xtender serial protocol appendix
    Date : 01.07.21
    Version : V1.6.28 (R676)

---
+++ XcomLAN/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This Package contains definition/implementation of an enhanced version of the hesso-valais/scom package
+to deal with Studer-Innotec Xcom-LAN/Xcom-232i nodes.
+
+Inspired from and Based on hesso-valais/scom
+<https://github.com/hesso-valais/scom/blob/0.7.3/>
+""" 
