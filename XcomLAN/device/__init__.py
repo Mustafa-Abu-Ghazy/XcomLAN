722dc45f1270d3fc276b016dd039e8421d8283c3
Mustafa M. A. U. AbuGhazy <moustafa.abu.ghazy@gmail.com>
2022-01-02 10:18:07 +0200

    Add DeviceFactory Class responsible for creating all the different devices
    This file contains definition/implementation of a DeviceFactory Class responsible for creating all the different devices
        - Xtender
        - VarioTrack
        - VarioString
        - RCC, Xcom-232i
        - Xcom-CAN
        - BSP
    Inspired from and Based on hesso-valais/scom : devicefactory.py
    <https://github.com/hesso-valais/scom/blob/0.7.3/src/sino/scom/device/devicefactory.py>

--- XcomLAN/device/__init__.py
+++ XcomLAN/device/__init__.py
@@ -10,6 +10,7 @@ Sub-Package contains Implementation of the all required device creation and repr
 from .addresses import *
 from .base_device import BaseDevice
 from .bsp import BSP
+from .device_factory import DeviceFactory
 from .device_type import DeviceType
 from .rcc import RCC
 from .vario_string import VarioString
