995ef5a1961b25406191b9437eda16d3ca171627
Mustafa M. A. U. AbuGhazy <moustafa.abu.ghazy@gmail.com>
2022-01-02 10:16:36 +0200

    Add definition/implementation of a Base-Class for all the different devices and Device-Class for each device type
    The Base-Class for all the different devices
        - Xtender
        - VarioTrack
        - VarioString
        - RCC, Xcom-232i
        - Xcom-CAN
        - BSP
    Inspired from and Based on hesso-valais/scom : scomdevice.py
    <https://github.com/hesso-valais/scom/blob/0.7.3/src/sino/scom/device/scomdevice.py>

--- XcomLAN/device/__init__.py
+++ XcomLAN/device/__init__.py
@@ -8,7 +8,13 @@ Sub-Package contains Implementation of the all required device creation and repr
 # -----------------------------------------------------  device   ---------------------------------------------------- #
 # -------------------------------------------------------------------------------------------------------------------- #
 from .addresses import *
+from .base_device import BaseDevice
+from .bsp import BSP
 from .device_type import DeviceType
+from .rcc import RCC
+from .vario_string import VarioString
+from .vario_track import VarioTrack
+from .xtender import Xtender
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------------  End of device   ------------------------------------------------- #
